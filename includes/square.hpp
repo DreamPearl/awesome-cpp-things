652cf2adbbed3dd9d4b62a03c6db276b29792b19
dreampearl <rakhi.cs61@gmail.com>
2021-08-07 20:06:43 +0530

    Demostrate conditional preprocessor directives

---
+++ includes/square.hpp
@@ -0,0 +1,23 @@
+// When a header file is included twice within a program, the compiler processes
+// the contents of that header file twice. This leads to an error in the
+// program. To eliminate this error, conditional preprocessor directives are
+// used.
+
+// SYNTAX:
+
+// #ifndef HEADER_FILE_NAME
+// #define HEADER_FILE_NAME
+
+//    the entire header file
+
+// #endif
+
+#ifndef SQUARE_HPP
+#define SQUARE_HPP
+
+int square(int num) {
+    int square = num * num;
+    return square;
+}
+
+#endif
---
+++ src/square.cpp
@@ -0,0 +1,13 @@
+// Oops I included the same header file twice,
+// Yeah yeah I can simply remove one of the header files.
+// But is it feasible to do so in large projects? Absolutely not!
+// So how to manage? Go and checkout the square.hpp header file for answer.
+
+#include "../includes/square.hpp"
+#include <iostream>
+
+int main() {
+    int num = 12;
+    std::cout << square(num) << std::endl;
+    return 0;
+}
