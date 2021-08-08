068c94cd31a7e1645573dcb54d4b3d8b53672b33
dreampearl <rakhi.cs61@gmail.com>
2021-08-09 00:09:27 +0530

    Formatting

--- includes/square.hpp
+++ includes/square.hpp
@@ -1,16 +1,17 @@
-// When a header file is included twice within a program, the compiler processes
-// the contents of that header file twice. This leads to an error in the
-// program. To eliminate this error, conditional preprocessor directives are
-// used.
+/* When a header file is included twice within a program, the compiler processes
+  the contents of that header file twice. This leads to an error in the
+  program. To eliminate this error, conditional preprocessor directives are
+  used.
 
-// SYNTAX:
+  SYNTAX:
 
-// #ifndef HEADER_FILE_NAME
-// #define HEADER_FILE_NAME
+  #ifndef HEADER_FILE_NAME
+  #define HEADER_FILE_NAME
 
-//    the entire header file
+     the entire header file
 
-// #endif
+  #endif
+*/
 
 #ifndef SQUARE_HPP
 #define SQUARE_HPP
--- src/square.cpp
+++ src/square.cpp
@@ -1,7 +1,8 @@
-// Oops I included the same header file twice,
-// Yeah yeah I can simply remove one of the header files.
-// But is it feasible to do so in large projects? Absolutely not!
-// So how to manage? Go and checkout the square.hpp header file for answer.
+/* Oops I included the same header file twice,
+ * Yeah yeah I can simply remove one of the header files.
+ * But is it feasible to do so in large projects? Absolutely not!
+ * So how to manage? Go and checkout the square.hpp header file for answer.
+ */
 
 #include "../includes/square.hpp"
 #include <iostream>
